b7015990dc86e5976ee851195e51ea74e01990e4
nourana23-meet <109900008+nourana23-meet@users.noreply.github.com>
2022-08-02 17:25:12 +0300

    this is the right file, sryyy, this is the finished one!

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,38 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = 'super-secret-key'
-
-
-@app.route('/', methods=['GET', 'POST'])
-def signin():
-    return render_template("signin.html")
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    error = ""
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        try:
-            login_session['user'] = python3auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('add_tweet.html'))
-        except:
-            error = "Authentication failed"
-    return render_template("signup.html")
-
-
-@app.route('/add_tweet', methods=['GET', 'POST'])
-def add_tweet():
-    return render_template("add_tweet.html")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 config = {
 
   "apiKey": "AIzaSyD01MX_zkp69Jf4qdqqRR83holC3xLDrB4",
@@ -50,5 +18,62 @@ config = {
 
   "measurementId": "G-6N0XJQ0C9T",
 
-  "databaseURL": ""
+  "databaseURL": "https://console.firebase.google.com/project/fir-6a332/database/fir-6a332-default-rtdb/data/~2F"
 } 
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = python3auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+    return render_template("signin.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = ""
+    if request.method == 'POST':
+        Full name = request.form['full name']
+        Name = request.form['name']
+        Bio = request.form['bio']
+        try:
+            login_session{'user'} = python3auth.create_user_with_email_and_password(email, password)
+            user = {"name": Name, "bio": Bio, "full name": Full name}
+            db.child("usera").child(login_session['user']['localId']).set(user)
+            return redirect(ur_for("add_tweet"))
+        except:
+            error = "Authentication failed"
+    return render_template("signup.html")
+
+
+@app.route('/add_tweet', methods=['GET', 'POST'])
+def add_tweet():
+    if request.method == "POST":
+        try:
+            tweet = {"Full name":request.form['full name'], "Name" :request.form['name'], "Bio": request.form['bio'], uid: ["localId"]}
+            db.child("Tweets").push(tweet)
+        except:
+           print("Couldn't add tweet, try again")
+    return render_template("add_tweet.html")
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweets():
+    display = db.child("Display").get().val()
+    return render_template("tweets.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
